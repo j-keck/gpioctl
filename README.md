ba7dea8be98d36470cceb63c80983168146ddcd1
j-keck <jhyphenkeck@gmail.com>
2016-03-15 20:21:42 +0100

    doc

---
+++ README.md
@@ -0,0 +1,7 @@
+# gpioctl - manage GPIO pins under bsd
+
+  * go library to manage GPIO pins
+  * used the 'ioctl()' system call to manage the gpio pins.
+  * tested under 11-CURRENT on a RPI-2
+  * no interrupts - currently not supported under freebsd
+  * check the 'examples' directory for examples
--- gpioctl.go
+++ gpioctl.go
@@ -1,3 +1,26 @@
+// Package gpioctl to manage GPIO pins under bsd
+//
+//
+// gpioctl use the 'ioctl()' system call to manage
+// the gpio pins.
+//
+//
+// EXAMPLE:
+//
+//   open pin 40 in output mode and set it to high:
+//
+//     if gpio, err := gpioctl.Open(); err == nil {
+//       defer gpio.Close()
+//
+//       if pin, err := gpio.Pin(40, gpioctl.GPIO_PIN_OUTPUT); err == nil {
+//         pin.Write(1)
+//       } else {
+//         panic(err)
+//       }
+//     } else {
+//       panic(err)
+//     }
+//
 package gpioctl
 
 
@@ -11,31 +34,47 @@ import "syscall"
 import "unsafe"
 
 
+// Gpio represents the GPIO bus
 type Gpio struct {
   fd int;
 }
 
+// Pin represents one GPIO Pin
 type Pin struct {
   nr   uint32;
   gpio Gpio;
 }
+
+
+// GPIO pin configuration flags
 type PinFlags uint32
 
 const (
+  // input direction
   GPIO_PIN_INPUT       PinFlags    = 0x0001;
+  // output direction
   GPIO_PIN_OUTPUT      PinFlags    = 0x0002;
+  // open-drain output
   GPIO_PIN_OPENDRAIN   PinFlags    = 0x0004;
+  // push-pull output
   GPIO_PIN_PUSHPULL    PinFlags    = 0x0008;
+  // output disabled
   GPIO_PIN_TRISTATE    PinFlags    = 0x0010;
+  // internal pull-up enabled
   GPIO_PIN_PULLUP      PinFlags    = 0x0020;
+  // internal pull-down enabled
   GPIO_PIN_PULLDOWN    PinFlags    = 0x0040;
+  // invert input
   GPIO_PIN_INVIN       PinFlags    = 0x0080;
+  // invert output
   GPIO_PIN_INVOUT      PinFlags    = 0x0100;
+  // pulsate in hardware
   GPIO_PIN_PULSATE     PinFlags    = 0x0200;
 )
 
 
 
+// open the GPIO bus
 func Open() (Gpio, error) {
   path := C._PATH_DEVGPIOC + "0"
   if fd, err := syscall.Open(path, syscall.O_RDONLY, 0); err != nil {
@@ -46,11 +85,13 @@ func Open() (Gpio, error) {
 }
 
 
+// close the GPIO bus
 func (gpio Gpio) Close() error {
   return syscall.Close(gpio.fd)
 }
 
 
+// configure a GPIO pin
 func (gpio Gpio) Pin(nr int, flags ...PinFlags) (Pin, error) {
  var gpiopin gpio_pin
   gpiopin.gp_pin = uint32(nr)
@@ -66,6 +107,7 @@ func (gpio Gpio) Pin(nr int, flags ...PinFlags) (Pin, error) {
 }
 
 
+// write the given value. 0 -> LOW, >= 1 -> HIGH
 func (pin Pin) Write(value int) error {
   var gpioreq gpio_req
   gpioreq.gp_pin = pin.nr
@@ -73,12 +115,16 @@ func (pin Pin) Write(value int) error {
   return ioctl(uintptr(pin.gpio.fd), C.GPIOSET, uintptr(unsafe.Pointer(&gpioreq)))
 }
 
+
+// toggle the current pin state
 func (pin Pin) Toggle() error {
   var gpioreq gpio_req
   gpioreq.gp_pin = pin.nr
   return ioctl(uintptr(pin.gpio.fd), C.GPIOTOGGLE, uintptr(unsafe.Pointer(&gpioreq)))
 }
 
+
+// read the current pin state
 func (pin Pin) Read() (int, error) {
   var gpioreq gpio_req
   gpioreq.gp_pin = pin.nr
@@ -88,6 +134,7 @@ func (pin Pin) Read() (int, error) {
     return int(gpioreq.gp_value), nil
   }
 }
+
 
 type gpio_pin struct {
   gp_pin   uint32;
